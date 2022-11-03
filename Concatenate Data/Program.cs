7b6453036271d9da23ec36aee6574e26f613b324
nanji <nanji@DESKTOP-4A8MIDK>
2022-11-03 21:35:27 +0300

    Add project files.

---
+++ Concatenate Data/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Concatenate_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Julii";
+            string second = "Abadjiev";
+            int age = 15;
+            string town = "Sofia";
+
+            Console.WriteLine($"You are {name} {second}, a {age} years old person from {town}");
+        }
+    }
+}
